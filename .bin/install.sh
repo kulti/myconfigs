92b38a74446210c034a07a29811795c3c2d073ab
Aleksey Bakin <alexey.bakin@gmail.com>
2020-11-23 19:47:16 +0300

    add iterm2 settings

--- .bin/install.sh
+++ .bin/install.sh
@@ -6,3 +6,7 @@ chmod 700 /usr/local/share/zsh/site-functions
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install pre-commit htop
+
+# setup iterm2
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
